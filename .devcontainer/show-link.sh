dd621665705cec2784a63783617ce84618e16b29
desmonsmilex-wq <desmonsmilex@gmail.com>
2026-05-11 12:35:19 +0330

    Update show-link.sh

--- .devcontainer/show-link.sh
+++ .devcontainer/show-link.sh
@@ -3,7 +3,7 @@ CONFIG="/etc/xray/g2ray.json"
 UUID=$(grep -o '"id": *"[^"]*"' "$CONFIG" | head -1 | grep -o '"[^"]*"$' | tr -d '"')
 if [ -z "$UUID" ]; then echo "[g2ray] UUID پیدا نشد."; exit 1; fi
 SNI="${CODESPACE_NAME}-443.app.github.dev"
-LINK="vless://${UUID}@94.130.50.12:443?encryption=none&security=tls&sni=${SNI}&host=${SNI}&fp=chrome&allowInsecure=1&type=xhttp&mode=packet-up&path=%2F#https://redesigned-barnacle-wv7qx5g7xqjr2v6g.github.dev/"
+LINK="vless://${UUID}@94.130.50.12:443?encryption=none&security=tls&sni=${SNI}&host=${SNI}&fp=chrome&allowInsecure=1&type=xhttp&mode=packet-up&path=%2F#https://redesigned-dollop-r7v55v7vw473wjp.github.dev/"
 echo ""
 echo "================================================"
 echo "  $LINK"
